551c7d5a7909d6163522c14c601b3882d55ad382
Strav <mathieu.beliveau.1@gmail.com>
2020-06-14 19:53:57 -0400

    Tried different way to create the dataset (lazy loading seems to be working fine)

--- bird_test.py
+++ bird_test.py
@@ -1,4 +1,30 @@
-import DataExtractor as de
+import pathlib
+import os
+import io
+import pandas as pd
+import numpy as np
+import tensorflow as tf
 
-extractor = de.DataExtractor()
+
+def transform_labels_to_one_hot_vector(labels):
+    labels_df = pd.DataFrame(labels)
+    return pd.get_dummies(labels_df, dtype=float).values
+
+
+def load_wav_data(file_path):
+    file_name = tf.strings.split(file_path, os.sep)[-1]
+    label_len = tf.strings.length(file_name) - 11
+    label = tf.strings.substr(file_name, 0, label_len)
+    wav = tf.io.read_file(file_path)
+    wav_tensor, sample_rate = tf.audio.decode_wav(wav)
+    return wav_tensor, label
+
+
+data_prefix_path = "../Bird_Songs/"
+meta_data = pd.read_csv(data_prefix_path + "metadata.csv")
+one_hot_labels = transform_labels_to_one_hot_vector(meta_data["Species"])
+dataset = tf.data.Dataset.list_files(data_prefix_path + "Wav/*")
+dataset = dataset.map(load_wav_data)
 x = 0
+
+
